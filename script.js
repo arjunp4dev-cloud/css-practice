ecb5439365845113fb79a4ce52f5cfde4eab01be
Arjun <arjunp4dev@gmail.com>
2026-03-12 15:59:47 +0530

    Updated files

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ function decrease(){
 
 }
 
+let tasks = ["Study JavaScript", "Build Project", "Practice Coding", "Learn Django"];
+
+let container = document.getElementById("taskContainer");
+
+for (let i = 0; i < tasks.length; i++) {
+
+    container.innerHTML += `
+        <div>
+            <p>${tasks[i]}</p>
+            <button onclick="deleteTask(this)">Delete</button>
+        </div>
+    `;
+}
+
+function deleteTask(button){
+    button.parentElement.remove();
+}
