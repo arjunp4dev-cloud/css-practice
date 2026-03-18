3818ffd9900d2c9cf0d76a720bf075438d6b1c20
Arjun <arjunp4dev@gmail.com>
2026-03-18 15:41:20 +0530

    Updated files

--- script.js
+++ script.js
@@ -132,3 +132,13 @@ document.getElementById("result").innerHTML = sum;
 
 
 
+/* backgroundcolor*/
+
+
+
+function Color() {
+    let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+}
+
+
